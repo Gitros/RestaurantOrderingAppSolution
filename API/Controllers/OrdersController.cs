dd238ab533defa502bdb50eb1b98adb996947991
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-10-23 16:06:18 +0200

    Adding an API controller

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+public class OrdersController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    public OrdersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Order>>> GetOrders()
+    {
+        return await _context.Orders.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Order>> GetOrder(int id)
+    {
+        return await _context.Orders.FindAsync(id);
+    }
+}
