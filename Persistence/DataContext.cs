2681605cadbf9ab605ca6757a0abed6728e1dbcc
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-10-23 14:13:50 +0200

    adding db context

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+}
