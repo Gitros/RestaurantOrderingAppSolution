63df25ad6228bfea68e285eaf4fab09de4741991
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-10-23 14:03:01 +0200

    creating a domain entities (order and orderitems)

---
+++ Domain/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string Table { get; set; }
+    public DateTime OrderDate { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
+    public decimal TotalAmount => OrderItems.Sum(item => item.Price * item.Quantity);
+}
