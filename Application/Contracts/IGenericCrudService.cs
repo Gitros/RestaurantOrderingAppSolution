e99d2a1a20a0c1f608b4a7acbfe097bd169aa18f
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-11-06 12:11:23 +0100

    generic crud testing

---
+++ Application/Contracts/IGenericCrudService.cs
@@ -0,0 +1,12 @@
+﻿using Application.Dtos.Common;
+using System.Linq.Expressions;
+
+namespace Application.Contracts;
+
+public interface IGenericCrudService<TModel, TDto>
+{
+    Task<ResultDto<TDto>> GetByIdAsync(Expression<Func<TModel, bool>> predicateToGetId, params string[] includes);
+    Task<ResultDto<IEnumerable<TDto>>> GetAllAsync(Expression<Func<TModel, bool>>? where = null, params string[] includes);
+    Task<ResultDto<TDto>> UpdateAsync(TDto dto, Expression<Func<TModel, bool>>? where = null, params Expression<Func<TModel, object>>[] references);
+    Task<ResultDto<bool>> DeleteAsync(Guid id);
+}
