7d10c301dfc9ed78be3c7f767b75a3a706e21829
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-11-17 16:27:57 +0100

    Adding BaseApiController with HandleResult

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Dtos.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class BaseApiController : ControllerBase
+{
+    protected ActionResult HandleResult<T>(ResultDto<T> result)
+    {
+        if (result == null)
+            return NotFound();
+
+        if (result.IsSuccess && result.Data != null)
+            return StatusCode((int)result.HttpStatusCode, result.Data);
+
+        if (result.IsSuccess && result.Data == null)
+            return NoContent();
+
+        return StatusCode((int)result.HttpStatusCode, new { result.ErrorMessage });
+    }
+}
