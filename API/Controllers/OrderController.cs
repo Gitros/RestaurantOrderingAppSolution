531dc44a756d8d9bb049332efd41ff8659266ccc
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-11-17 16:41:59 +0100

    Updating OrderController with HandleResult

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -1,85 +1,41 @@
 ﻿using Application.Contracts;
-using Application.Dtos.Common;
 using Application.Dtos.Orders;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class OrderController : ControllerBase
+namespace API.Controllers
 {
-    private readonly IOrderService _orderService;
-
-    public OrderController(IOrderService orderService)
+    public class OrderController : BaseApiController
     {
-        _orderService = orderService;
-    }
+        private readonly IOrderService _orderService;
 
-    [HttpPost]
-    public async Task<IActionResult> CreateOrder([FromBody] OrderCreateDto orderCreateDto)
-    {
-        var result = await _orderService.CreateOrder(orderCreateDto);
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
 
-        if (result.IsSuccess)
-            return StatusCode((int)result.HttpStatusCode, result.Data);
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder([FromBody] OrderCreateDto orderCreateDto) =>
+            HandleResult(await _orderService.CreateOrder(orderCreateDto));
 
-        return StatusCode((int)result.HttpStatusCode, result.ErrorMessage);
-    }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrder(Guid id) =>
+            HandleResult(await _orderService.GetOrder(id));
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetOrder(Guid id)
-    {
-        var result = await _orderService.GetOrder(id);
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrders() =>
+            HandleResult(await _orderService.GetAllOrders());
 
-        if (result.IsSuccess)
-            return Ok(result.Data);
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateOrder([FromBody] OrderUpdateDto orderUpdateDto, Guid id) =>
+            HandleResult(await _orderService.UpdateOrder(orderUpdateDto, id));
 
-        return StatusCode((int)result.HttpStatusCode, result.ErrorMessage);
-    }
+        [HttpPut("{id}/status")]
+        public async Task<IActionResult> UpdateOrderStatus([FromBody] OrderStatus newStatus, Guid id) =>
+            HandleResult(await _orderService.UpdateOrderStatus(newStatus, id));
 
-    [HttpGet]
-    public async Task<IActionResult> GetAllOrders()
-    {
-        var result = await _orderService.GetAllOrders();
-
-        if (result.IsSuccess)
-            return Ok(result.Data);
-
-        return StatusCode((int)result.HttpStatusCode, result.ErrorMessage);
-    }
-
-    [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateOrder([FromBody] OrderUpdateDto orderUpdateDto, Guid id)
-    {
-        var result = await _orderService.UpdateOrder(orderUpdateDto, id);
-
-        if (result.IsSuccess)
-            return Ok(result.Data);
-
-        return StatusCode((int)result.HttpStatusCode, result.ErrorMessage);
-    }
-
-    [HttpPut("{id}/status")]
-    public async Task<IActionResult> UpdateOrderStatus([FromBody] OrderStatus newStatus, Guid id)
-    {
-        var result = await _orderService.UpdateOrderStatus(newStatus, id);
-
-        if (result.IsSuccess)
-            return Ok(result.Data);
-
-        return StatusCode((int)result.HttpStatusCode, result.ErrorMessage);
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteOrder(Guid id)
-    {
-        var result = await _orderService.DeleteOrder(id);
-
-        if (result.IsSuccess)
-            return StatusCode((int)result.HttpStatusCode, result.Data);
-
-        return StatusCode((int)result.HttpStatusCode, result.ErrorMessage);
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(Guid id) =>
+            HandleResult(await _orderService.DeleteOrder(id));
     }
 }
