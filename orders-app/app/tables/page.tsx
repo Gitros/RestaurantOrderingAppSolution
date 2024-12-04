ebc669f59e11c53c13be80418a5ce281997dd867
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-12-04 12:14:11 +0100

    table page start

--- orders-app/app/tables/page.tsx
+++ orders-app/app/tables/page.tsx
@@ -1,7 +1,14 @@
 export default function Index() {
   return (
-    <div>
-      <h1> dldsa</h1>
+    <div className="flex-1 flex flex-col bg-white rounded-r-lg">
+      <div className="p-5">
+        <h1 className="text-xl font-bold">Wybierz stolik</h1>
+        <div className="flex space-x-4 mt-4">
+          <button className="px-4 py-2 bg-[#1E2A38] text-white rounded-md hover:bg-[#2B4B5C]">
+            Bar
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
